65bc76e6dd81e9fda2a0efcfd1bd7137a6c45ad6
Shreyas Jejurkar <shreyasjejurkar123@live.com>
2021-04-24 21:28:32 +0530

    Initial commit

---
+++ src/Constant.cs
@@ -0,0 +1,54 @@
+﻿using System.IO;
+using System.Text;
+
+namespace Project
+{
+    public static class Constant
+    {
+        #region Private fields
+
+        private static readonly string _solutionFileContent = @"Microsoft Visual Studio Solution File, Format Version 12.00
+# Visual Studio Version 16
+VisualStudioVersion = 16.0.31220.234
+MinimumVisualStudioVersion = 10.0.40219.1
+Global
+	GlobalSection(SolutionProperties) = preSolution
+		HideSolutionNode = FALSE
+	EndGlobalSection
+	GlobalSection(ExtensibilityGlobals) = postSolution
+		SolutionGuid = {346B8E55-4F98-411A-B190-929B790EC1AA}
+	EndGlobalSection
+EndGlobal";
+
+        private static readonly string _gitignoreContent = @"[Oo]bj/
+[Bb]in/
+.nuget/
+_ReSharper.*
+packages/
+artifacts/
+*.user
+*.suo
+*.userprefs
+*DS_Store
+*.sln.ide";
+
+        private static readonly string _nugetConfigContent = @"<?xml version=""1.0"" encoding=""utf-8""?>
+<configuration>
+  <packageSources>
+    <!--To inherit the global NuGet package sources remove the <clear/> line below -->
+    <clear />
+    <add key=""nuget"" value=""https://api.nuget.org/v3/index.json"" />
+  </packageSources>
+</configuration>
+";
+        #endregion
+
+        private static readonly string _buildCmdContent = @$"dotnet build";
+
+        public static readonly string DirectoryPath = Directory.GetCurrentDirectory();
+        public static readonly byte[] SolutionFileContentInBytes = Encoding.UTF8.GetBytes(_solutionFileContent);
+        public static readonly byte[] GitIgnoreFileContentInBytes = Encoding.UTF8.GetBytes(_gitignoreContent);
+        public static readonly byte[] BuildCmdContentInBytes = Encoding.UTF8.GetBytes(_buildCmdContent);
+        public static readonly byte[] NugetConfigContentInBytes = Encoding.UTF8.GetBytes(_nugetConfigContent);
+    }
+}
---
+++ src/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Project
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            Console.WriteLine("Initializing new project...");
+            string fullPath = Path.GetFullPath(Directory.GetCurrentDirectory()).TrimEnd(Path.DirectorySeparatorChar);
+            string projectName = Path.GetFileName(fullPath);
+
+            await CreateFile($"{projectName}.sln", content: Constant.SolutionFileContentInBytes);
+            await CreateFile(".gitignore", content: Constant.GitIgnoreFileContentInBytes);
+            await CreateFile(".gitattributes");
+            await CreateFile("build.cmd", content: Constant.BuildCmdContentInBytes);
+            await CreateFile("build.sh", content: Constant.BuildCmdContentInBytes);
+            await CreateFile("LICENSE");
+            await CreateFile("NuGet.Config", content: Constant.NugetConfigContentInBytes);
+            await CreateFile("README.md");
+            CreateFolder("src");
+            CreateFolder("artifacts");
+            CreateFolder("build");
+            CreateFolder("docs");
+            CreateFolder("lib");
+            CreateFolder("packages");
+            CreateFolder("samples");
+            CreateFolder("tests");
+
+            Console.WriteLine($"Project structure has been created. Click on {projectName}.sln to open solution.");
+        }
+
+        private static async Task CreateFile(string fileName, byte[] content = null)
+        {
+            using FileStream fs = File.Create(Path.Combine(Constant.DirectoryPath, fileName));
+
+            if (content is not null)
+            {
+                await fs.WriteAsync(content);
+            }
+        }
+
+        private static void CreateFolder(string folderName)
+        {
+            Directory.CreateDirectory(Path.Combine(Constant.DirectoryPath, folderName));
+        }
+    }
+}
